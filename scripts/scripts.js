f18e24ec9705cefab39503183dcc55247b7f17d3
brianpocasangre <brianpocasangre1@gmail.com>
2024-04-24 23:19:48 -0700

    add button and getSize function for onClick event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
+function getSize() {
+  let n = prompt('Chose any number between 1 and 100 to regenerate the grid?');
+  createDivs(n);
+}
+
 const main = document.querySelector('#main');
 
 function createDivs(n) {
+  main.innerHTML = ` `;
   for (i = 0; i < n; i++) {
     for (e = 0; e < n; e++) {
       main.innerHTML += `<div class="grid"></div>`;
